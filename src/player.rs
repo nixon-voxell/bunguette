a9c5046b54f6bb1d4732f842d291b361010a816f
Nixon <43715558+nixon-voxell@users.noreply.github.com>
2025-05-24 23:20:06 +0800

    Add player possession

--- src/player.rs
+++ src/player.rs
@@ -7,11 +7,196 @@ pub(super) struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_observer(setup_name_ui_for_player)
-            .add_observer(setup_player_tag);
+        app.init_state::<PlayerState>()
+            .init_resource::<PlayerPossession>()
+            .add_observer(setup_name_ui_for_player)
+            .add_observer(setup_player_tag)
+            .add_systems(
+                OnEnter(PlayerState::Possessing),
+                setup_possessing_ui,
+            )
+            .add_systems(
+                Update,
+                process_posessing_inputs
+                    .run_if(in_state(PlayerState::Possessing)),
+            );
 
         app.register_type::<PlayerType>();
     }
+}
+
+fn process_posessing_inputs(
+    q_gamepads: Query<(&Gamepad, Entity)>,
+    kbd_inputs: Res<ButtonInput<KeyCode>>,
+    mut player_possession: ResMut<PlayerPossession>,
+) {
+    if kbd_inputs.just_pressed(KeyCode::KeyA) {
+        // Remove previous possession if any.
+        if player_possession.player_b == Some(Possession::Keyboard) {
+            player_possession.player_b = None;
+        }
+
+        // Assign current possesion.
+        player_possession.player_a = Some(Possession::Keyboard);
+    }
+
+    if kbd_inputs.just_pressed(KeyCode::KeyD) {
+        // Remove previous possession if any.
+        if player_possession.player_a == Some(Possession::Keyboard) {
+            player_possession.player_a = None;
+        }
+
+        // Assign current possesion.
+        player_possession.player_b = Some(Possession::Keyboard);
+    }
+
+    // Handle cancelation.
+    if kbd_inputs.just_pressed(KeyCode::Escape) {
+        if player_possession.player_a == Some(Possession::Keyboard) {
+            player_possession.player_a = None;
+        }
+
+        if player_possession.player_b == Some(Possession::Keyboard) {
+            player_possession.player_b = None;
+        }
+    }
+
+    for (gamepad, entity) in q_gamepads.iter() {
+        if gamepad.just_pressed(GamepadButton::DPadLeft) {
+            // Remove previous possession if any.
+            if player_possession.player_b
+                == Some(Possession::Gamepad(entity))
+            {
+                player_possession.player_b = None;
+            }
+
+            // Assign current possesion.
+            player_possession.player_a =
+                Some(Possession::Gamepad(entity));
+        }
+
+        if gamepad.just_pressed(GamepadButton::DPadRight) {
+            // Remove previous possession if any.
+            if player_possession.player_a
+                == Some(Possession::Gamepad(entity))
+            {
+                player_possession.player_a = None;
+            }
+
+            // Assign current possesion.
+            player_possession.player_b =
+                Some(Possession::Gamepad(entity));
+        }
+
+        // Handle cancelation.
+        if gamepad.just_pressed(GamepadButton::East) {
+            if player_possession.player_a
+                == Some(Possession::Gamepad(entity))
+            {
+                player_possession.player_a = None;
+            }
+
+            if player_possession.player_b
+                == Some(Possession::Gamepad(entity))
+            {
+                player_possession.player_b = None;
+            }
+        }
+    }
+}
+
+fn setup_possessing_ui(mut commands: Commands) {
+    let instruction_ui_node = Node {
+        justify_content: JustifyContent::Center,
+        align_items: AlignItems::Center,
+        padding: UiRect::all(Val::VMin(6.0)),
+        flex_grow: 1.0,
+        flex_direction: FlexDirection::Column,
+        ..default()
+    };
+
+    let instruction_content_ui = children![
+        (
+            instruction_ui_node.clone(),
+            children![(
+                Text::new(
+                    "Press:\nA (keyboard) / DPadLeft (controller)\n...to possess Player A",
+                ),
+                // TextLayout::new_with_justify(JustifyText::Center),
+            )],
+        ),
+        // Separation line.
+        (
+            Node {
+                width: Val::Px(10.0),
+                height: Val::Percent(80.0),
+                ..default()
+            },
+            BackgroundColor(GRAY_200.into()),
+        ),
+        (
+            instruction_ui_node,
+            children![
+                Text::new(
+                    "Press:\nD (keyboard) / DPadRight (controller)\n...to possess Player B",
+                ),
+                // TextLayout::new_with_justify(JustifyText::Center),
+            ],
+        ),
+    ];
+
+    let instruction_ui = children![
+        (
+            Text::new(
+                "Press Esc (keyboard) | B (controller) to cancel."
+            ),
+            TextLayout::new_with_justify(JustifyText::Center),
+        ),
+        (
+            Node {
+                flex_direction: FlexDirection::Row,
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                flex_grow: 1.0,
+                ..default()
+            },
+            instruction_content_ui,
+        )
+    ];
+
+    commands.spawn((
+        StateScoped(PlayerState::Possessing),
+        Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            padding: UiRect::all(Val::VMin(10.0)),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        },
+        // Should be on top of all other uis.
+        GlobalZIndex(10),
+        children![(
+            Node {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                flex_direction: FlexDirection::Column,
+                padding: UiRect::all(Val::VMin(4.0)),
+                flex_grow: 1.0,
+                ..default()
+            },
+            BackgroundColor(ZINC_950.with_alpha(0.8).into()),
+            BorderRadius::all(Val::VMin(4.0)),
+            BoxShadow::new(
+                ZINC_950.with_alpha(0.9).into(),
+                Val::Auto,
+                Val::Auto,
+                Val::Px(20.0),
+                Val::Px(16.0),
+            ),
+            instruction_ui,
+        )],
+    ));
 }
 
 /// Setup world space name ui for players.
@@ -102,3 +287,25 @@ pub struct PlayerA;
 /// A unique component tag for player B.
 #[derive(Component, Debug)]
 pub struct PlayerB;
+
+#[derive(Clone, Eq, PartialEq, Debug, Hash, Default, States)]
+#[states(scoped_entities)]
+pub enum PlayerState {
+    #[default]
+    Possessing,
+    _Possessed,
+}
+
+/// The currently possession state of the players.
+#[derive(Resource, Default, Debug)]
+pub struct PlayerPossession {
+    pub player_a: Option<Possession>,
+    pub player_b: Option<Possession>,
+}
+
+/// Possesion state, can be keyboard or a specific gamepad.
+#[derive(Debug, PartialEq, Eq)]
+pub enum Possession {
+    Keyboard,
+    Gamepad(Entity),
+}
